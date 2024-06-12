631e4f96f74ce8fe7170c4106509b34e6b73c2bc
ritup <promitm83@gmail.com>
2024-06-13 00:52:03 +0530

    1st commit to developNew

--- src/test/java/com/UtilityPkg/ConfigClassTestCase.java
+++ src/test/java/com/UtilityPkg/ConfigClassTestCase.java
@@ -6,9 +6,15 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ConfigClassTestCase {
-
+    protected WebDriver driver;
+    protected ExtentReports extent;
+    protected ExtentTest test;
 
 
     public WebDriver configMethod(){
@@ -19,6 +25,14 @@ public class ConfigClassTestCase {
         //driver.get("https://psdocenecqasrv.rnd.metricstream.com/metricstream/auth/signin.jsp");
         driver.manage().window().maximize();
 
+        /*ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("test-report.html");
+        htmlReporter.config().setDocumentTitle("Automation Test Report");
+        htmlReporter.config().setReportName("Functional Test Report");
+        htmlReporter.config().setTheme(Theme.STANDARD);
+
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+*/
         return driver;
     }
 
